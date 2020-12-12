367f0cdc439e7bdeb219aaa82a1ce8fda312929d
Dale Emery <dale@dhemery.com>
2020-12-12 12:32:11 -0800

    Add Assertion alias

--- dheunit/assertions.h
+++ dheunit/assertions.h
@@ -6,30 +6,28 @@
 namespace dhe {
 namespace unit {
 
-template <typename Actual>
-auto is_equal_to(Actual const &want)
-    -> std::function<void(Tester &, Actual const &)> {
-  return [want](Tester &t, Actual const &actual) {
+template <typename T>
+using Assertion = std::function<void(Tester &, T const &)>;
+
+template <typename T> auto is_equal_to(T const &want) -> Assertion<T> {
+  return [want](Tester &t, T const &actual) {
     if (actual != want) {
       t.errorf("was {}, want a value equal to {}", actual, want);
     }
   };
 }
 
-template <typename Actual>
-auto is_not_equal_to(Actual const &want)
-    -> std::function<void(Tester &, Actual const &)> {
-  return [want](Tester &t, Actual const &actual) {
+template <typename T> auto is_not_equal_to(T const &want) -> Assertion<T> {
+  return [want](Tester &t, T const &actual) {
     if (actual == want) {
       t.errorf("was {}, want a value not equal to {}", actual, want);
     }
   };
 }
 
-template <typename Actual>
-auto is_near(Actual const &want, Actual const &tolerance)
-    -> std::function<void(Tester &, Actual const &)> {
-  return [want, tolerance](Tester &t, Actual const &actual) {
+template <typename T>
+auto is_near(T const &want, T const &tolerance) -> Assertion<T> {
+  return [want, tolerance](Tester &t, T const &actual) {
     auto const min = want - tolerance;
     auto const max = want + tolerance;
     if (actual < min || actual > max) {
@@ -38,50 +36,41 @@ auto is_near(Actual const &want, Actual const &tolerance)
   };
 }
 
-template <typename Actual>
-auto is_greater_than(Actual const &max)
-    -> std::function<void(Tester &, Actual const &)> {
-  return [max](Tester &t, Actual const &actual) {
+template <typename T> auto is_greater_than(T const &max) -> Assertion<T> {
+  return [max](Tester &t, T const &actual) {
     if (!(actual > max)) {
       t.errorf("was {}, want greater than {}", actual, max);
     }
   };
 }
 
-template <typename Actual>
-auto is_no_greater_than(Actual const &max)
-    -> std::function<void(Tester &, Actual const &)> {
-  return [max](Tester &t, Actual const &actual) {
+template <typename T> auto is_no_greater_than(T const &max) -> Assertion<T> {
+  return [max](Tester &t, T const &actual) {
     if (actual > max) {
       t.errorf("was {}, want no greater than {}", actual, max);
     }
   };
 }
 
-template <typename Actual>
-auto is_between(Actual const &min, Actual const &max)
-    -> std::function<void(Tester &, Actual const &)> {
-  return [min, max](Tester &t, Actual const &actual) {
+template <typename T>
+auto is_between(T const &min, T const &max) -> Assertion<T> {
+  return [min, max](Tester &t, T const &actual) {
     if (actual < min || actual > max) {
       t.errorf("was {}, want between {} and {}", actual, min, max);
     }
   };
 }
 
-template <typename Actual>
-auto is_less_than(Actual const &min)
-    -> std::function<void(Tester &, Actual const &)> {
-  return [min](Tester &t, Actual const &actual) {
+template <typename T> auto is_less_than(T const &min) -> Assertion<T> {
+  return [min](Tester &t, T const &actual) {
     if (!(actual < min)) {
       t.errorf("was {}, want less than {}", actual, min);
     }
   };
 }
 
-template <typename Actual>
-auto is_no_less_than(Actual const &min)
-    -> std::function<void(Tester &, Actual const &)> {
-  return [min](Tester &t, Actual const &actual) {
+template <typename T> auto is_no_less_than(T const &min) -> Assertion<T> {
+  return [min](Tester &t, T const &actual) {
     if (actual < min) {
       t.errorf("was {}, want no less than {}", actual, min);
     }
