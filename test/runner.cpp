80c51c36712da9e3a38122be91f0a46d4a87f8f9
Dale Emery <dale@dhemery.com>
2020-12-11 21:46:21 -0800

    Clean up test runner

--- test/runner.cpp
+++ test/runner.cpp
@@ -1,5 +1,5 @@
-#include "dheunit/verbose-test-runner.h"
+#include <dheunit/runner.h>
 
-using dhe::unit::runner::VerboseTestRunner;
+using dhe::unit::runner::run_tests;
 
-auto main() -> int { VerboseTestRunner{}.run(); }
+auto main() -> int { run_tests(std::cout); }
